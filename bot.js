5e28c5813ef0562b489cd844f3ff025150814250
Max Antinone <mantinone@gmail.com>
2018-03-15 19:14:46 -0700

    Removed timer fxns

--- bot.js
+++ bot.js
@@ -20,22 +20,6 @@ bot.on('ready', function (evt) {
   logger.info(bot.username + ' - (' + bot.id + ')')
 })
 
-setInterval(function() {
-  bot.sendMessage({
-    to: '418499444858683394',
-    message: `This is a full-day interval, ${clock.checkDate()}`
-  })
-}, 25*60*60*1000)
-
-setInterval(function() {
-  if( clock.checkNewDay() ){
-    bot.sendMessage({
-      to: '418499444858683394',
-      message: `Hourly interval check has worked, ${clock.checkDate()}`
-    })
-  }
-}, 60*60*1000)
-
 bot.on('message', function (user, userID, channelID, message, evt) {
   if (message.substring(0,1) == '!'){
     var args = message.substring(1).split(' ')
