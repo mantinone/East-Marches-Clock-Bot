a4e0dcd5b184ca013668ae18cf04ac0e5f5e4ec6
Max Antinone <mantinone@gmail.com>
2018-06-21 00:13:33 -0700

    Fixed test version of timer

--- bot.js
+++ bot.js
@@ -25,24 +25,23 @@ setInterval( () => {
     to: '427869441741684748',
     message: clock.printDate()
   })
-  if( clock.checkCrafting()){
-    bot.sendMessage({
-      to: '422936057101680640',
-      message: 'Hello @everyone!  A new crafting week has begun!'
-    })
-  }
-  // let info = clock.testCrafting())
-  // console.log(info);
-  // if( info.craftTime ){
-  //   let minutes = 60 - info.minutes
-  //   setTimeout( () => {
-  //     bot.sendMessage({
-  //       to: '423358604444172289',
-  //       message: "Here's hoping this is on the hour!"
-  //     }, 1000*60*minutes)
+  // if( clock.checkCrafting()){
+  //   bot.sendMessage({
+  //     to: '422936057101680640',
+  //     message: 'Hello @everyone!  A new crafting week has begun!'
   //   })
-
-  //}
+  // }
+  let info = clock.testCrafting()
+  //console.log(info);
+  if( info.craftTime ){
+    let minutes = 60 - info.minutes
+    setTimeout( () => {
+      bot.sendMessage({
+        to: '423358604444172289',
+        message: "This hopefully posts on the hour."
+      })
+    }, 1000*60*minutes)
+  }
 },  1000*60*30)
 
 bot.on('message', function (user, userID, channelID, message, evt) {
@@ -72,12 +71,14 @@ bot.on('message', function (user, userID, channelID, message, evt) {
           message: `!roll 1d20 + ${bonus||0} Rolling Insight for <@${userID}> because <@225782923490492417> did or said something.`
         })
         break
-      // case "test":
-      //   bot.sendMessage({
-      //     to: '423358604444172289',
-      //     message: "How do I do this?  @everyone"
-      //   })
-      //   break
+      case "test":
+        let info = clock.testCrafting()
+        console.log(info);
+        bot.sendMessage({
+          to: '423358604444172289',
+          message: `${info.craftTime} ${info.minute}`
+        })
+        break
     }
   }
 })
